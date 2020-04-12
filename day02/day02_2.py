f55a2e65b7ed6c2593ff6c8b147c4f032a26a0f9
Allen <tl230419@163.com>
2020-04-12 14:07:00 +0800

    add day02_2.py file

---
+++ day02/day02_2.py
@@ -0,0 +1,21 @@
+'''
+row = 1
+while row < 10:
+    col = 1
+    while col <= row:
+        #print('*', end='')
+        print('%d * %d = %d'%(col, row, col*row), end='\t')
+        col += 1
+    print()
+    row += 1
+'''
+
+row = 9
+while row > 0:
+    col = 1
+    while col <= row:
+        # print('*', end='')
+        print('%d * %d = %d' % (col, row, col * row), end='\t')
+        col += 1
+    print()
+    row -= 1
